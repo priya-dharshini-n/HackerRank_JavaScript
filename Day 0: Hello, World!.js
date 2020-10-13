a77ea61020527aaea04e78ee57d432b91a58b95c
PriyaaaDharshini <priyabesonatech@gmail.com>
2020-10-13 21:11:42 +0530

    Question
    A greeting function is provided for you in the editor below. It has one parameter,
    
    . Perform the following tasks to complete this challenge:
    
        Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
        Use console.log() to print the contents of 
    
        (i.e., the argument passed to main).
    
    You've got this!

---
+++ Day 0: Hello, World!.js
@@ -0,0 +1,14 @@
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+    //ne of code that prints parameterVariable to stdout using console.log:
+    
+}
+
+
+function main() {
+    const parameterVariable = readLine();
+    
+    greeting(parameterVariable);
+}
