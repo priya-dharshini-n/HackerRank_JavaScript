17811bbc0da5408ed3ebc06c656b4ef57951b39d
PriyaaaDharshini <priyabesonatech@gmail.com>
2020-10-13 21:15:25 +0530

    Question
    Variables named , , and are declared for you in the editor below. You must use the
    
    operator to perform the following sequence of operations:
    
        Convert 
    
    to an integer (Number type), then sum it with
    and print the result on a new line using console.log.
    Convert
    to a floating-point number (Number type), then sum it with
    and print the result on a new line using console.log.
    Print the concatenation of
    and on a new line using console.log. Note that must be printed first.

---
+++ Day_0:_Data_Types.js
@@ -0,0 +1,22 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    
+    const firstDecimal = 4.0;
+    console.log(firstInteger + parseInt(secondInteger))
+    console.log(firstDecimal + parseFloat(secondDecimal))
+    
+
+    const firstString = 'HackerRank ';
+    console.log(firstString + secondString)
+  
+}
+
+
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    
+    performOperation(secondInteger, secondDecimal, secondString);
+}
